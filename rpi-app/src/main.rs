b31b94f2a3be21181ae698ad29afd9838a399a18
Harry Phillips <harryjamesphillips@gmail.com>
2023-01-25 16:15:56 +0000

    Reading temperature sensor in rust

--- rpi-app/src/main.rs
+++ rpi-app/src/main.rs
@@ -1,3 +1,17 @@
+use std::thread::sleep;
+use std::time::Duration;
+use i2cdev::core::I2CDevice;
+use i2cdev::linux::LinuxI2CDevice;
+use byteorder::{BigEndian, ByteOrder};
+
 fn main() {
     println!("Hello, world!");
+
+    let mut dev = LinuxI2CDevice::new("/dev/i2c-1", 0x40).unwrap();
+    dev.write(&[0xF3]).unwrap();
+    sleep(Duration::from_secs(1));
+    let mut data = [0u8; 2];
+    dev.read(&mut data).unwrap();
+    let data_parsed = BigEndian::read_u16(&data);
+    println!("Result: {data_parsed}");
 }

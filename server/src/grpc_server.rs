c0c0d20f1cbec29e24ea672fc0ed6f552377ea8c
Harry Phillips <harryjamesphillips@gmail.com>
2023-01-26 00:32:39 +0000

    Implemented basic data send / receive

--- server/src/grpc_server.rs
+++ server/src/grpc_server.rs
@@ -1,43 +1,39 @@
+use tokio::sync::mpsc::Sender;
 use tokio::task::JoinHandle;
-use tonic::{Request, Response, Status};
+use tonic::{Request, Response, Status, Streaming};
 use tonic::transport::Server;
-use crate::grpc_server::server::greeter_server::{Greeter, GreeterServer};
-use crate::grpc_server::server::{HelloReply, HelloRequest};
+use nodeapi::node_api_server::{NodeApi, NodeApiServer};
+use futures_util::StreamExt;
+use crate::grpc_server::nodeapi::{Empty, EnvironmentData};
 
-pub fn launch() -> JoinHandle<()> {
-    tokio::spawn(start_server())
+pub fn launch(data_sink: Sender<(f32, f32)>) -> JoinHandle<()> {
+    tokio::spawn(start_server(data_sink))
 }
 
-async fn start_server() {
-    let addr = "[::]:50051".parse().unwrap();
-    let greeter = MyGreeter::default();
-
+async fn start_server(data_sink: Sender<(f32, f32)>) {
     println!("Starting gRPC Server on http://localhost:50051");
     Server::builder()
-        .add_service(GreeterServer::new(greeter))
-        .serve(addr)
+        .add_service(NodeApiServer::new(NodeApiImpl { data_sink }))
+        .serve("[::]:50051".parse().unwrap())
         .await.unwrap();
 }
 
-mod server {
-    tonic::include_proto!("server");
+mod nodeapi {
+    tonic::include_proto!("nodeapi");
 }
 
-#[derive(Debug, Default)]
-pub struct MyGreeter {}
+pub struct NodeApiImpl {
+    data_sink: Sender<(f32, f32)>
+}
 
 #[tonic::async_trait]
-impl Greeter for MyGreeter {
-    async fn say_hello(
-        &self,
-        request: Request<HelloRequest>, // Accept request of type HelloRequest
-    ) -> Result<Response<HelloReply>, Status> { // Return an instance of type HelloReply
-        println!("Got a request: {:?}", request);
-
-        let reply = server::HelloReply {
-            message: format!("Hello {}!", request.into_inner().name).into(), // We must use .into_inner() as the fields of gRPC requests and responses are private
-        };
-
-        Ok(Response::new(reply)) // Send back our formatted greeting
+impl NodeApi for NodeApiImpl {
+    async fn report_environment(&self, request: Request<Streaming<EnvironmentData>>) -> Result<Response<Empty>, Status> {
+        let mut stream = request.into_inner();
+        while let Some(data_result) = stream.next().await {
+            let data = data_result?;
+            self.data_sink.send((data.temperature, data.relative_humidity)).await.unwrap();
+        }
+        Ok(Response::new(nodeapi::Empty{}))
     }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,26 @@
+use std::sync::Arc;
+use tokio::spawn;
+use tokio::sync::{mpsc, RwLock};
+
 mod http_server;
 mod grpc_server;
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
-    let http_server_handle = http_server::launch();
-    let grpc_server_handle = grpc_server::launch();
+    let (data_in, mut data_out) = mpsc::channel(1);
+    let lock = Arc::new(RwLock::new(Vec::new()));
 
+    let http_server_handle = http_server::launch(lock.clone());
+    let grpc_server_handle = grpc_server::launch(data_in);
+
+    let data_handler_handle = spawn(async move {
+        while let Some(data) = data_out.recv().await {
+            lock.write().await.push(data);
+        }
+    });
+
+    // TODO shutdown if any service stops
     http_server_handle.await.unwrap();
     grpc_server_handle.await.unwrap();
+    data_handler_handle.await.unwrap();
 }

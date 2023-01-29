40b38d81ac1c3f00ca062386ad51db3b01b0517c
Liam Gallagher <37050440+Gallagator@users.noreply.github.com>
2023-01-29 17:39:37 +0000

    Add basic support for an sqlite database with SeaORM (#23)
    * Add basic database code connection for sqlite
    
    * Use Diesel to create a basic database.
    
    * Revert accidental server changes
    
    * Remove src/schema.rs from root of src
    
    * Move all database stuff to database module directory.
    
    * Switch to SeaORM database as it's less awful than diesel
    
    * Maybe fix Dockerfile
    
    * Fixedish Dockerfile
    
    * Docker rust caching
    
    * Small Dockerfile Tweak
    
    * Caching didn't work, remove it
    
    ---------
    
    Co-authored-by: Harry Phillips <harryjamesphillips@gmail.com>

---
+++ server/src/db/entities.rs
@@ -0,0 +1,1 @@
+include!(concat!(env!("OUT_DIR"), "/db/entities.rs"));
---
+++ server/src/db/migrations/initial.rs
@@ -0,0 +1,45 @@
+use sea_orm_migration::prelude::*;
+
+pub struct Migration;
+
+impl MigrationName for Migration {
+    fn name(&self) -> &str {
+        "initial"
+    }
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Node::Table)
+                    .col(
+                        ColumnDef::new(Node::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Node::Latitude).double().not_null())
+                    .col(ColumnDef::new(Node::Longitude).double().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Node::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(Iden)]
+pub enum Node {
+    Table,
+    Id,
+    Latitude,
+    Longitude,
+}

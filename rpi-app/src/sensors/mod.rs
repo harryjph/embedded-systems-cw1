29fa85ed4267c93f4e033fdb86cb5bddc6b5f664
Harry Phillips <harryjamesphillips@gmail.com>
2023-01-30 18:31:52 +0000

    Refactor temp/humid driver

--- rpi-app/src/sensors/mod.rs
+++ rpi-app/src/sensors/mod.rs
@@ -8,10 +8,12 @@ pub type Result<T> = result::Result<T, Box<dyn Error>>;
 
 #[async_trait]
 pub trait TemperatureSensor {
+    /// Reads the temperature in degrees celsius
     async fn read_temperature(&mut self) -> Result<f32>;
 }
 
 #[async_trait]
 pub trait HumiditySensor {
+    /// Reads the relative humidity as a percentage
     async fn read_humidity(&mut self) -> Result<f32>;
 }

a1b4e1d06211b943808261afdccafa7952f43129
Liam Gallagher <37050440+Gallagator@users.noreply.github.com>
2023-02-01 12:36:19 +0000

    Add burst read untested but idc (#35)

--- rpi-app/src/sensors/mod.rs
+++ rpi-app/src/sensors/mod.rs
@@ -24,3 +24,11 @@ pub trait ProximitySensor {
     /// Reads the proximity in meters
     async fn read_proximity(&mut self) -> Result<f32>;
 }
+
+async fn average_proximity<T: ProximitySensor>(sensor: &mut T, num_readings: usize) -> Result<f32> {
+    let mut sum = 0.0f32;
+    for _ in 0..num_readings {
+        sum += sensor.read_proximity().await?;
+    }
+    Ok(sum / num_readings as f32)
+}

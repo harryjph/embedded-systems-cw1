abdfdfade848f24ed12ed73b4df16bc4bd0c6680
OrkunAliOzkan <orkunozkan@outlook.com>
2023-02-03 17:05:44 +0000

    Cleaning BinsList.js

--- frontend/src/Components/Bins/BinsList.js
+++ frontend/src/Components/Bins/BinsList.js
@@ -11,14 +11,11 @@ function BinsList(props) {
     let binsWidgets = props.AllData.map(bin => 
                                             (<Bins 
                                                 key={bin.id} 
-                                                
                                                 ID={bin.id} 
-                                                /*  ****  */
                                                 Name={bin.config.name} 
                                                 Latitude={bin.config.latitude} 
                                                 Longitude={bin.config.longitude} 
                                                 Threshold={bin.config.full_threshold}
-                                                /*  ****  */
                                                 Fullness={Math.floor(bin.fullness * 100)}/>
                                             )
                                         );

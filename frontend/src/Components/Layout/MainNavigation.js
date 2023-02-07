1f682316bc79cc87898e5bcb0491980a799ace9a
Harry Phillips <harryjamesphillips@gmail.com>
2023-02-07 18:19:32 +0000

    Fix icon link

--- frontend/src/Components/Layout/MainNavigation.js
+++ frontend/src/Components/Layout/MainNavigation.js
@@ -21,7 +21,7 @@ function MainNavigation() {
     <div>
       <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
         <div className="container flex flex-wrap items-center justify-between mx-auto">
-          <a href="/my-bins" className="flex items-center">
+          <a href="/app/my-bins" className="flex items-center">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-10" alt="Logo" />
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">IoT101</span>
           </a>

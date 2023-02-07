3df41de40b7369c4ff3f05270144e48c296d2080
Harry Phillips <harryjamesphillips@gmail.com>
2023-02-07 18:05:57 +0000

    Implement auto-login & logout

--- frontend/src/Components/Layout/MainNavigation.js
+++ frontend/src/Components/Layout/MainNavigation.js
@@ -1,6 +1,13 @@
-import { Link } from "react-router-dom";
+import {apiPostForm} from "../../API";
 
 function MainNavigation() {
+  function logout() {
+    apiPostForm("/user/logout", {}, true)
+      .then((_) => {
+        window.location = "/app";
+      });
+  }
+
   return (
     <div>
       <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
@@ -11,12 +18,12 @@ function MainNavigation() {
           </a>
 
           <div className="flex md:order-2">
-            <Link
-              to="/"
+            <button
+              onClick={logout}
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Sign out
-            </Link>
+            </button>
           </div>
 
           <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">

6d6bc8d5d31dea82630c5286458d210e2a59759b
OrkunAliOzkan <orkunozkan@outlook.com>
2023-02-07 12:05:08 +0000

    release/claim Yes/No buttons look slightly less shi

--- frontend/src/Components/BasicComponents/ModalUserDefined.js
+++ frontend/src/Components/BasicComponents/ModalUserDefined.js
@@ -24,20 +24,21 @@ function ModalUserDefined(props) {
 
   return (
     <div className="modal">
-    {console.log(props.isOpen)}
       <div className="text-center">
         <HiOutlineExclamationCircle className="z-1 mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
         <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
           Are you sure?
         </h3>
-        <div className="flex items-center justify-center">
+        <div className="flex justify-center items-center">
           <Button
-            className="text-white bg-[#C81E1E] hover:bg-[#9B1C1C]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2"
+            className="text-white bg-[#C81E1E] hover:bg-[#9B1C1C]/90 
+            focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-1 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2
+            "
             onClick={YesSelect}
             >Yes
           </Button>
           <Button
-            className="text-white bg-[#6B7280] hover:bg-[#4B5563]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2"
+            className="text-white bg-[#6B7280] hover:bg-[#4B5563]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-1 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2"
             onClick={NoSelect}
           >Cancel
           </Button>

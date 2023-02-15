86345dd64f210b9436e8ea1590f232f17deaf366
Harry Phillips <harryjamesphillips@gmail.com>
2023-02-15 15:10:10 +0000

    Make it clear which tab is selected

--- frontend/src/Components/Layout/MainNavigation.js
+++ frontend/src/Components/Layout/MainNavigation.js
@@ -20,6 +20,12 @@ function MainNavigation() {
     });
   }
 
+  const page = window.location.pathname.split("/").pop();
+
+  const tabClasses = "block py-2 pl-3 pr-4 text-gray-700 text-xl rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
+  const myBinsClasses = page === "my-bins" ? tabClasses + " font-bold underline" : tabClasses;
+  const unownedBinsClasses = page === "unowned-bins" ? tabClasses + " font-bold underline" : tabClasses;
+
   return (
     <div>
       <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
@@ -30,12 +36,10 @@ function MainNavigation() {
           </a>
 
           <div className="flex md:order-2">
-            {/* <UserProfile email={email} Signout={logout}/> */}
 
             <Dropdown
               onClick={() => setAvatarDropdown(!avatarDropdown)}
               label={<AiOutlineUser />}
-              // dismissOnClick={false}
             >
               {avatarDropdown && (
                 <div className="">
@@ -51,14 +55,6 @@ function MainNavigation() {
                 </div>
               )}
             </Dropdown>
-
-            {/* <p>Logged in as {email}</p>
-              <button
-                onClick={logout}
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Sign out
-              </button>  */}
           </div>
 
           <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
@@ -66,7 +62,7 @@ function MainNavigation() {
               <li>
                 <a
                   href="/app/my-bins"
-                  className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className={myBinsClasses}
                 >
                   My Bins
                 </a>
@@ -74,7 +70,7 @@ function MainNavigation() {
               <li>
                 <a
                   href="/app/unowned-bins"
-                  className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className={unownedBinsClasses}
                 >
                   Unowned Bins
                 </a>

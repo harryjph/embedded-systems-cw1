828664b2e1453417b93744ea8bc3107fabaaaeb8
Harry Phillips <harryjamesphillips@gmail.com>
2023-02-15 17:02:03 +0000

    Run prettier

--- frontend/src/Components/Bins/BinsList.js
+++ frontend/src/Components/Bins/BinsList.js
@@ -13,30 +13,31 @@ function BinsList(props) {
 
   function SeeRoutingMap() {
     const successCallback = (position) => {
-      apiPostJson("/bins/route", { starting_latitude: position.coords.latitude, starting_longitude: position.coords.longitude })
+      apiPostJson("/bins/route", {
+        starting_latitude: position.coords.latitude,
+        starting_longitude: position.coords.longitude,
+      })
         .then((r) => r.json())
         .then((response) => {
           const binIds = response.route;
           let mapData = props.AllData.filter((bin) => binIds.includes(bin.id));
           setMapData(mapData);
           let InitialLatLng = [position.coords.latitude, position.coords.longitude];
-          let LatterLatLongData = mapData.map(
-            (bin) => {
-              return [bin.config.latitude, bin.config.longitude]
-            }
-          );
+          let LatterLatLongData = mapData.map((bin) => {
+            return [bin.config.latitude, bin.config.longitude];
+          });
           let LatLongData = [InitialLatLng].concat(LatterLatLongData);
           setRouteData(LatLongData);
           //  end
           setSeeMap(true);
         });
     };
-    
+
     const errorCallback = (error) => {
       alert("Error getting location: " + error);
     };
-    
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);    
+
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
   }
 
   function functionSeeMap(mapIds) {
@@ -94,7 +95,7 @@ function BinsList(props) {
   });
   return (
     <div>
-      {SeeMap && <MapModal AllData={MapData} RoutingData={RouteData}/>}
+      {SeeMap && <MapModal AllData={MapData} RoutingData={RouteData} />}
       <div className="flex items-center justify-center m-5">
         <button
           className="m-1 inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
--- frontend/src/Components/Map/Routing.js
+++ frontend/src/Components/Map/Routing.js
@@ -1,28 +1,27 @@
 import L from "leaflet";
-import {createControlComponent} from "@react-leaflet/core";
+import { createControlComponent } from "@react-leaflet/core";
 import "leaflet-routing-machine";
 
-const createRoutineMachineLayer = ({RoutingData}) => {
-    const returnWayPointArr = () => {
-        const waypoints = [];
-        for(let i = 0; i < RoutingData.length; i++) {
-            waypoints[i] = L.latLng(RoutingData[i][0], RoutingData[i][1]);
-        }
-    return waypoints;
-        
+const createRoutineMachineLayer = ({ RoutingData }) => {
+  const returnWayPointArr = () => {
+    const waypoints = [];
+    for (let i = 0; i < RoutingData.length; i++) {
+      waypoints[i] = L.latLng(RoutingData[i][0], RoutingData[i][1]);
     }
+    return waypoints;
+  };
   return L.Routing.control({
-      waypoints: returnWayPointArr(),
-      lineOptions: {
-        styles: [{color: "#6FA1EC", weight: 4}]
-      },
-      show: false,
-      addWaypoints: false,
-      routeWhileDragging: true,
-      draggableWaypoints: true,
-      fitSelectedRoutes: true,
-      showAlternatives: false
-    });
+    waypoints: returnWayPointArr(),
+    lineOptions: {
+      styles: [{ color: "#6FA1EC", weight: 4 }],
+    },
+    show: false,
+    addWaypoints: false,
+    routeWhileDragging: true,
+    draggableWaypoints: true,
+    fitSelectedRoutes: true,
+    showAlternatives: false,
+  });
 };
 
 const Routing = createControlComponent(createRoutineMachineLayer);

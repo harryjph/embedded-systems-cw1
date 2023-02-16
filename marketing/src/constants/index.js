9d3b0d6f4e851839bcb483c93202eb10ce030547
Harry Phillips <harryjamesphillips@gmail.com>
2023-02-16 17:11:11 +0000

    Some code cleanup

--- marketing/src/constants/index.js
+++ marketing/src/constants/index.js
@@ -53,31 +53,6 @@ export const features = [
     content: "",
   },
 ];
-
-export const stuff = [
-  {
-    id: "feature-1",
-    icon: star,
-    title: "Advanced Routing Algorithm ",
-    content:
-      "Our advanced routing algorithm employs real-time data analytics to determine which bins require immediate attention, optimizing waste collection and disposal.",
-  },
-  {
-    id: "feature-2",
-    icon: star,
-    title: "High Scalability",
-    content:
-      "Our algorithm is designed to handle a large number of bins, without any loss of efficiency or performance. You can scale our technology to suit your needs, regardless of the size of your waste management network. Choose us and experience the benefits of a high-performance waste management system that can adapt to your evolving needs.",
-  },
-  {
-    id: "feature-3",
-    icon: shield,
-    title: "Secure Network",
-    content:
-      "Rest easy knowing that your data is always protected with our state-of-the-art security measures. Our network features TLS encryption to safeguard your information, while your password is secured by 10,000 PBKDF2 iterations and salting. We take your privacy seriously and are committed to keeping your data safe and secure. ",
-  },
-];
-
 export const feedback = [
   {
     id: "feedback-1",
@@ -122,30 +97,6 @@ export const stats = [
     value: "230M+",
   },
 ];
-
-export const socialMedia = [
-  {
-    id: "social-media-1",
-    icon: instagram,
-    link: "https://www.instagram.com/",
-  },
-  {
-    id: "social-media-2",
-    icon: facebook,
-    link: "https://www.facebook.com/",
-  },
-  {
-    id: "social-media-3",
-    icon: twitter,
-    link: "https://www.twitter.com/",
-  },
-  {
-    id: "social-media-4",
-    icon: linkedin,
-    link: "https://www.linkedin.com/",
-  },
-];
-
 export const clients = [
   {
     id: "client-1",
